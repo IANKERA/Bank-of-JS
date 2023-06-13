da92bc37e0080eabfe8fd80f1b29b95a5ca69f55
Your Name <you@example.com>
2023-06-13 07:49:57 +0300

    challenge2

--- script.js
+++ script.js
@@ -300,3 +300,17 @@ btnSort.addEventListener('click', function (e) {
 //   console.log(newJuia);
 //   console.log(juliaKate);
 // };
+
+//coding challenge 4!!
+
+const dogs = [
+  { weight: 22, curFood: 250, owners: ['Alice', 'Bob'] },
+  { weight: 8, curFood: 200, owners: ['Matilda'] },
+  { weight: 13, curFood: 275, owners: ['Sarah', 'John'] },
+  { weight: 32, curFood: 340, owners: ['Michael'] },
+];
+
+dogs.forEach(add => (add.recomendedFood = Math.trunc(add.weight ** 0.75 * 28)));
+
+const sarahdog = dogs.find(dog => dog.owners.includes('Sarah'));
+console.log(sarahdog);
